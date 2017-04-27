278e119feb9ecf0e8495486555fb9055f6fb47be
Kareem Ergawy <kareem_ergawy@yahoo.com>
2017-04-27 12:41:20 +0200

    removing unneeded stuff

--- openmp/merge-sort/OMPMergeSortParallel_Sections.cpp
+++ openmp/merge-sort/OMPMergeSortParallel_Sections.cpp
@@ -1,34 +1,3 @@
-/*
-  Copyright (C) 2014 Intel Corporation
-  All rights reserved.
-
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions
-  are met:
-
-  * Redistributions of source code must retain the above copyright
-    notice, this list of conditions and the following disclaimer.
-  * Redistributions in binary form must reproduce the above copyright
-    notice, this list of conditions and the following disclaimer in
-    the documentation and/or other materials provided with the
-    distribution.
-  * Neither the name of Intel Corporation nor the names of its
-    contributors may be used to endorse or promote products derived
-    from this software without specific prior written permission.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-  OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-  AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
-  WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-  POSSIBILITY OF SUCH DAMAGE.
-*/
 #include "OMPMergeSortParallel.h"
 #include <algorithm>
 #include <iostream>
@@ -39,43 +8,6 @@
 namespace pss {
 
 namespace internal {
-
-// Merge sequences [xs,xe) and [ys,ye) to output sequence
-// [zs,zs+(xe-xs)+(ye-ys)) Destroy input sequence iff destroy==true
-// void parallel_merge(int *xs, int *xe, int *ys, int *ye, int *zs, bool
-// destroy) {
-//   const size_t MERGE_CUT_OFF = 2000;
-//   while ((xe - xs) + (ye - ys) > MERGE_CUT_OFF) {
-//     int *xm;
-//     int *ym;
-//     if (xe - xs < ye - ys) {
-//       ym = ys + (ye - ys) / 2;
-//       xm = std::upper_bound(xs, xe, *ym);
-//     } else {
-//       xm = xs + (xe - xs) / 2;
-//       ym = std::lower_bound(ys, ye, *xm);
-//     }
-
-//     if (omp_get_num_threads() == 1) {
-// #pragma omp parallel
-//       {
-// #pragma omp master
-//         {
-// #pragma omp task
-//           { parallel_merge(xs, xm, ys, ym, zs, destroy); }
-//         }
-//       }
-//     } else {
-// #pragma omp task
-//       { parallel_merge(xs, xm, ys, ym, zs, destroy); }
-//     }
-//     zs += (xm - xs) + (ym - ys);
-//     xs = xm;
-//     ys = ym;
-//   }
-//   serial_merge(xs, xe, ys, ye, zs);
-// #pragma omp taskwait
-// }
 void parallel_merge(int *xs, int *xe, int *ys, int *ye, int *zs, bool destroy) {
   const size_t MERGE_CUT_OFF = 2000;
   if ((xe - xs) + (ye - ys) > MERGE_CUT_OFF) {
